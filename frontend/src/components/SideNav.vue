484b3c9e6bae54118b73e5384ee52abcc1774f02
jakeyjakeyy <jrichca1@gmail.com>
2024-04-20 21:24:24 -0700

    side-nav basic

---
+++ frontend/src/components/SideNav.vue
@@ -0,0 +1,27 @@
+<!-- Stream, Chat, About <user> -->
+<script setup lang="ts"></script>
+
+<template>
+  <div class="side-nav">
+    <aside class="menu">
+      <p class="menu-label">Routes</p>
+      <ul class="menu-list">
+        <li>
+          <router-link to="/" exact>Home</router-link>
+        </li>
+      </ul>
+      <p class="menu-label">Following</p>
+      <ul class="menu-list">
+        <li>
+          <router-link to="/stream">Stream</router-link>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<style>
+.menu {
+  width: 240px;
+}
+</style>
---
+++ frontend/src/views/StreamView.vue
@@ -0,0 +1,44 @@
+<!-- Stream, Chat, About <user> -->
+<script setup lang="ts">
+import SideNav from "@/components/SideNav.vue";
+import { ref, onMounted } from "vue";
+
+const width = ref(0);
+const height = ref(0);
+
+const updateDimensions = () => {
+  width.value = window.innerWidth;
+  height.value = window.innerHeight;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", updateDimensions);
+  updateDimensions();
+});
+</script>
+
+<template>
+  <div class="row">
+    <SideNav />
+    <div class="stream">
+      <video-player
+        src="http://localhost/live/test/index.m3u8"
+        controls
+        :width="width"
+        :height="height"
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+.row {
+  display: flex;
+  flex-direction: row;
+}
+.stream {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
